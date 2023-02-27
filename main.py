5dc77312c6fdc8f738865cdcd0d1970fe88d67e4
ST0M4 <125476397+ST0M4@users.noreply.github.com>
2023-02-27 17:41:40 +0200

    lesson notes

---
+++ main.py
@@ -0,0 +1,61 @@
+# first_value = 6
+# second_value = 9
+# print(first_value < second_value)
+# print(first_value <= second_value)
+# print(first_value > second_value)
+# print(first_value >= second_value)
+# print(first_value == second_value)
+# print(first_value != second_value)
+import datetime
+
+# test_str_1 = 'abc'
+# test_str_2 = 'abcd'
+# print(test_str_1 < test_str_2)
+# print(test_str_1 <= test_str_2)
+# print(test_str_1 > test_str_2)
+# print(test_str_1 >= test_str_2)
+# print(test_str_1 == test_str_2)
+# print(test_str_1 != test_str_2)
+
+# first_date = datetime.datetime.now()
+# second_date = datetime.datetime(2023, 2, 19)
+# print(first_date < second_date)
+# print(first_date <= second_date)
+# print(first_date > second_date)
+# print(first_date >= second_date)
+# print(first_date == second_date)
+# print(first_date != second_date)
+# print(first_date)
+# print(second_date)
+
+
+# and or in
+# and
+# true true -> true
+# true false -> false
+# false true -> false
+# false false -> false
+
+# or
+# true true -> true
+# true false -> true
+# false true -> true
+# false false -> false
+
+# not
+# true -> false
+# false -> true
+
+# print(7 < 10) and (6 < 9)
+# print(3 < 5) and (16 < 9)
+# print(3 == 5) or (16 == 16)
+# print((2 + 2 ==4) and not (2 + 2 == 5) and (2 + 2 == 2 * 2))
+
+while True:
+    x = int(input("Enter value: "))
+    if x > 10:
+        print("result is bigger than x")
+    elif x == 10:
+        print("Result is equal to 10")
+    else:
+        print("result is less than x")
