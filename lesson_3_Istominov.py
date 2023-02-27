0893d8837bafd1fee8b809c97c6b1d626c7b2897
ST0M4 <125476397+ST0M4@users.noreply.github.com>
2023-02-27 17:50:58 +0200

    simplifying some functions

--- lesson_3_Istominov.py
+++ lesson_3_Istominov.py
@@ -4,13 +4,13 @@ import random
 # Task 1
 
 min = random.randint(0, 60)
-if min >= 0 and min <= 14:
+if 0 <= min <= 14:
     print(min)
     print("First quarter")
-elif min >= 15 and min <= 30:
+elif 15 <= min <= 30:
     print(min)
     print("Second quarter")
-elif min >=31 and min <= 45:
+elif 31 <= min <= 45:
     print(min)
     print("Third quarter")
 else:
@@ -41,7 +41,7 @@ else:
 # Task 3
 
 number = random.randint(1, 999)
-list_digits = list(map(int, str(number))) # x = [int(a) for a in str(number)]
+list_digits = list(map(int, str(number)))  # Був ще такий варіант, x = [int(a) for a in str(number)]
 sum_digits = sum(list_digits)
 if (sum_digits % 3 == 0) and (list_digits[-1] % 2 == 0):
     print(f'Число {number} ділиться на 6')
