72226634cb23173619fdc80f90c1f4933009e416
ST0M4 <125476397+ST0M4@users.noreply.github.com>
2023-04-14 01:52:07 +0300

    Submitting homework

---
+++ Lesson_14_Decorators_and_ListComprehension/lesson_14_Istominov.py
@@ -0,0 +1,33 @@
+import random
+from collections import Counter
+
+
+# Task 1
+def print_name(func):
+    def inner(a, b):
+        print(func.__name__)
+        result = func(a, b)
+        return result
+    return inner
+
+
+@print_name
+def add_func(a, b):
+    return a + b
+
+
+@print_name
+def mult_func(a, b):
+    return a * b
+
+
+print(add_func(2, 3))
+print(mult_func(2, 3))
+
+# Task 2
+comprehension_list = [random.randint(1, 10) for item in range(100)]
+c = Counter(comprehension_list)
+# OR
+count_list = {i: comprehension_list.count(i) for i in comprehension_list}
+print(c)
+print(count_list)
